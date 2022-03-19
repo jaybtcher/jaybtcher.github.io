d2e3919e930fbcc3a26ad188ae9549838d67f9aa
jaySimz99 <58327702+jaySimz99@users.noreply.github.com>
2022-03-19 15:48:37 +0000

    Update index.js

--- client/index.js
+++ client/index.js
@@ -14,3 +14,13 @@ function ReadAppend(file, appendFile){
     });
 
     }
+
+    if ('serviceWorker' in navigator) {
+        console.log('service worker works');
+        window.addEventListener('load', () => {
+          navigator.serviceWorker
+          .register('/serviceWorker.js')
+          .then(reg => console.log('service worker registered'))
+          .catch(err => console.log('Error: ${err}')) 
+        })//setting up the service worker
+    }
