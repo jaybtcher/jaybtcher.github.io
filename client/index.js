181f040eaceaf97f47fd772b390918e0dca38937
jaySimz99 <58327702+jaySimz99@users.noreply.github.com>
2022-03-19 21:20:05 +0000

    Update index.js

--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator) {
     console.log('service worker works');
     window.addEventListener('load', () => {
         navigator.serviceWorker
-            .register('/serviceWorker.js')
+            .register('serviceWorker.js')
             .then(reg => console.log('service worker registered'))
             .catch(err => console.log('Error: ${err}'))
     })//setting up the service worker
