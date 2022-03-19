72757d14ba8e1bd20d5be5c4376e0790f3abdd2f
jaySimz99 <58327702+jaySimz99@users.noreply.github.com>
2022-03-19 20:57:42 +0000

    changes

--- client/index.js
+++ client/index.js
@@ -1,21 +1,3 @@
-var fs = require('fs');
-
-function ReadAppend(file, appendFile) {
-    fs.readFile(appendFile, function (err, data) {
-
-        if (err) throw err;
-        console.log('File was read');
-
-        fs.appendFile(file, data, function (err) {
-            if (err) throw err;
-            console.log('The "data to append" was appended to file!')
-        });
-
-    });
-
-}
-
-
 if ('serviceWorker' in navigator) {
     console.log('service worker works');
     window.addEventListener('load', () => {
